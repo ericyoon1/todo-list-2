9dc3d3bfeed2ba20edfb621d02f1ad509a4aed62
ericyoon1 <eriqyoon@gmail.com>
2017-11-08 12:52:20 -0800

    Completed fetching and rendering data from the server

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import {applyMiddleware} from 'redux';
 import rootReducer from './reducers';   //from './reducers/index';
+import promise from 'redux-promise';
 
 import App from './components/app';
 
 ReactDOM.render(
-    <Provider store={createStore(rootReducer, {}, applyMiddleware())}>
+    <Provider store={createStore(rootReducer, {}, applyMiddleware(promise))}>
         <App/>
     </Provider>,
     document.getElementById('root')
